b3c727b5801c4f6e3ddff729427dd5f355422fd5
Randall Mardus <randalljmardus@gmail.com>
2016-02-11 13:26:39 -0500

    first commit

--- GuardStatements/ViewController.swift
+++ GuardStatements/ViewController.swift
@@ -13,6 +13,46 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    
+        struct Customer {
+            var name: String?
+            var age: Int?
+            var phone: Int?
+            
+            func isValid() {
+                
+                guard let validName = name else {
+                    print("Invalid name")
+                    return
+                }
+                
+                guard let validAge = age where age > 0 else {
+                    print("Invalid age")
+                    return
+                }
+                
+                guard let validPhone = phone else {
+                    print("Invalid phone")
+                    return
+                }
+                
+                print("Our customer is valid: ", validName, validAge, validPhone)
+            }
+        }
+        
+        var firstCustomer = Customer()
+        firstCustomer.isValid()
+        
+        firstCustomer.name = "Marvin"
+        firstCustomer.phone = 5551212
+        firstCustomer.age = 0
+        firstCustomer.isValid()
+        
+        firstCustomer.age = 32
+        firstCustomer.isValid()
+        
+    
+    
     }
 
     override func didReceiveMemoryWarning() {
